b27761123d312b4686d5a65a753e84a1a42cf49d
Saimun Hasan <saimunhasanrifat14@gmail.com>
2025-04-12 23:27:39 +0600

    modify hero section

--- src/Components/HomeComponents/Hero.jsx
+++ src/Components/HomeComponents/Hero.jsx
@@ -4,8 +4,19 @@ import Nav from "./Nav";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
-import sliderimg from "../../assets/HomePageIMG/profile.jpg"
+import sliderimg from "../../assets/HomePageIMG/profile.jpg";
+import image1 from "../../assets/HomePageIMG/image1.jpg";
+import image2 from "../../assets/HomePageIMG/image2.jpg";
+import image3 from "../../assets/HomePageIMG/image3.jpg";
+import image4 from "../../assets/HomePageIMG/image4.jpg";
+import image5 from "../../assets/HomePageIMG/image5.jpg";
 
+import img1 from "../../assets/HomePageIMG/img1.jpg";
+import img2 from "../../assets/HomePageIMG/img2.jpg";
+import img3 from "../../assets/HomePageIMG/img3.jpg";
+import img4 from "../../assets/HomePageIMG/img4.jpg";
+import img5 from "../../assets/HomePageIMG/img5.jpg";
+import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 
 const Hero = () => {
   return (
@@ -15,101 +26,259 @@ const Hero = () => {
           <Nav />
         </div>
         <div>
-          <div className="h-[100vh] w-full flex items-center justify-between px-15 relative">
-            <div className="w-[50%] flex flex-col gap-7">
-              <h1 className="text-[70px] leading-[75px]">
-                Empower <br /> Connections <br /> with ChatRoom
-              </h1>
-              <p className="w-[50%]">
-                Lorem ipsum dolor,{" "}
-                <span className="text-gray-500">sit amet consectetur</span>{" "}
-                adipisicing elit. Ipsa ipsam dolor cumque{" "}
-                <span className="text-[#80cf9c]"> expedita</span>{" "}
-              </p>
-              <div className="flex gap-5">
-                <button className="bg-black text-white pl-4 p-1 rounded-3xl flex items-center cursor-pointer">
-                  Try out!{" "}
-                  <span className="p-2 text-[22px] ml-2 bg-white rounded-full text-black">
-                    <MdOutlineArrowOutward />
-                  </span>
-                </button>
-                <button className="bg-gray-100 pl-4 p-1 rounded-3xl flex items-center cursor-pointer">
-                  Learn more{" "}
-                  <span className="p-2 text-[22px] ml-2 bg-white rounded-full ">
-                    <MdOutlineArrowOutward />
-                  </span>
-                </button>
+          <div className="h-[100vh] w-full flex items-center justify-between px-20 relative">
+            <div className="w-[60%] flex items-center justify-between">
+              <div className="flex flex-col gap-7">
+                <h1 className="text-[70px] leading-[75px]">
+                  Empower <br /> Connections <br /> with ChatRoom
+                </h1>
+                <p className="w-[50%]">
+                  Lorem ipsum dolor,{" "}
+                  <span className="text-gray-500">sit amet consectetur</span>{" "}
+                  adipisicing elit. Ipsa ipsam dolor cumque{" "}
+                  <span className="text-[#80cf9c]"> expedita</span>{" "}
+                </p>
+                <div className="flex gap-5">
+                  <button className="bg-black text-white pl-4 p-1 rounded-3xl flex items-center cursor-pointer">
+                    Try out!{" "}
+                    <span className="p-2 text-[22px] ml-2 bg-white rounded-full text-black">
+                      <MdOutlineArrowOutward />
+                    </span>
+                  </button>
+                  <button className="bg-gray-100 pl-4 p-1 rounded-3xl flex items-center cursor-pointer">
+                    Learn more{" "}
+                    <span className="p-2 text-[22px] ml-2 bg-white rounded-full ">
+                      <MdOutlineArrowOutward />
+                    </span>
+                  </button>
+                </div>
               </div>
             </div>
-            <div className="w-[40%] h-full flex">
-              <div className="w-[50%] h-full overflow-hidden">
-                <Swiper
-                  direction="vertical"
-                  modules={[Autoplay]}
-                  autoplay={{
-                    delay: 0,
-                    disableOnInteraction: false,
-                  }}
-                  speed={2000} // long speed for smoothness
-                  loop={true}
-                  allowTouchMove={true}
-                  slidesPerView={3}
-                  spaceBetween={0}
-                  className="h-full w-ful flex flex-col gap-5"
-                >
-                  <SwiperSlide>
-                    <div className="text-white text-2xl my-4 p-10"><div className="py-5"><img className="w-[100%] h-[240px] rounded-3xl" src={sliderimg} alt="" /></div></div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="text-white text-2xl my-4 p-10"><div className="py-5"><img className="w-[100%] h-[240px] rounded-3xl" src={sliderimg} alt="" /></div></div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="text-white text-2xl my-4 p-10"><div className="py-5"><img className="w-[100%] h-[240px] rounded-3xl" src={sliderimg} alt="" /></div></div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="text-white text-2xl my-4 p-10"><div className="py-5"><img className="w-[100%] h-[240px] rounded-3xl" src={sliderimg} alt="" /></div></div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="text-white text-2xl my-4 p-10"><div className="py-5"><img className="w-[100%] h-[240px] rounded-3xl" src={sliderimg} alt="" /></div></div>
-                  </SwiperSlide>
-                </Swiper>
+            <div className="w-[40%] h-full flex items-center justify-between">
+              <div className="w-full h-full flex">
+                <div className="w-[50%] h-full overflow-hidden">
+                  <Swiper
+                    direction="vertical"
+                    modules={[Autoplay]}
+                    autoplay={{
+                      delay: 0,
+                      disableOnInteraction: false,
+                      pauseOnMouseEnter: true,
+                    }}
+                    speed={2000} // long speed for smoothness
+                    loop={true}
+                    allowTouchMove={true}
+                    slidesPerView={3}
+                    spaceBetween={0}
+                    className="h-full w-ful "
+                  >
+                    <SwiperSlide>
+                      <div className="text-white  my-4 p-10">
+                        <div className="py-5">
+                          <img
+                            className="w-[100%] h-[240px] rounded-3xl"
+                            src={image1}
+                            alt=""
+                          />
+                          <button className="bg-black  sliderButton w-[65%] text-white p-1 pl-5 rounded-3xl flex items-center justify-between cursor-pointer">
+                            Learn more{" "}
+                            <span className="p-1 text-[22px] ml-2 bg-white rounded-full text-black cursor-pointer">
+                              <MdOutlineArrowOutward />
+                            </span>
+                          </button>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <div className="text-white my-4 p-10">
+                        <div className="py-5">
+                          <img
+                            className="w-[100%] h-[240px] rounded-3xl"
+                            src={image2}
+                            alt=""
+                          />
+                          <button className="bg-black  sliderButton w-[65%] text-white p-1 pl-5 rounded-3xl flex items-center justify-between cursor-pointer">
+                            Learn more{" "}
+                            <span className="p-1 text-[22px] ml-2 bg-white rounded-full text-black cursor-pointer">
+                              <MdOutlineArrowOutward />
+                            </span>
+                          </button>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <div className="text-white  my-4 p-10">
+                        <div className="py-5">
+                          <img
+                            className="w-[100%] h-[240px] rounded-3xl"
+                            src={image3}
+                            alt=""
+                          />
+                          <button className="bg-black  sliderButton w-[65%] text-white p-1 pl-5 rounded-3xl flex items-center justify-between cursor-pointer">
+                            Learn more{" "}
+                            <span className="p-1 text-[22px] ml-2 bg-white rounded-full text-black cursor-pointer">
+                              <MdOutlineArrowOutward />
+                            </span>
+                          </button>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <div className="text-white  my-4 p-10">
+                        <div className="py-5">
+                          <img
+                            className="w-[100%] h-[240px] rounded-3xl"
+                            src={image4}
+                            alt=""
+                          />
+                          <button className="bg-black  sliderButton w-[65%] text-white p-1 pl-5 rounded-3xl flex items-center justify-between cursor-pointer">
+                            Learn more{" "}
+                            <span className="p-1 text-[22px] ml-2 bg-white rounded-full text-black cursor-pointer">
+                              <MdOutlineArrowOutward />
+                            </span>
+                          </button>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <div className="text-white my-4 p-10">
+                        <div className="py-5">
+                          <img
+                            className="w-[100%] h-[240px] rounded-3xl"
+                            src={image5}
+                            alt=""
+                          />
+                          <button className="bg-black  sliderButton w-[65%] text-white p-1 pl-5 rounded-3xl flex items-center justify-between cursor-pointer">
+                            Learn more{" "}
+                            <span className="p-1 text-[22px] ml-2 bg-white rounded-full text-black cursor-pointer">
+                              <MdOutlineArrowOutward />
+                            </span>
+                          </button>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                  </Swiper>
+                </div>
+                <div className="w-[50%] h-full overflow-hidden">
+                  <Swiper
+                    direction="vertical"
+                    modules={[Autoplay]}
+                    autoplay={{
+                      delay: 0,
+                      disableOnInteraction: false,
+                      pauseOnMouseEnter: true,
+                    }}
+                    speed={2000}
+                    loop={true}
+                    allowTouchMove={true}
+                    slidesPerView={3}
+                    spaceBetween={0}
+                    className="h-full w-full"
+                    style={{
+                      transform: "rotate(180deg)",
+                      transformOrigin: "center",
+                    }}
+                  >
+                    <SwiperSlide>
+                      <div className="p-10 ">
+                        <div className="py-5">
+                          <img
+                            className="w-[100%] h-[240px] rounded-3xl"
+                            src={img1}
+                            alt=""
+                          />
+                          <button className="bg-black  sliderRightButton w-[65%] text-white p-1 pl-5 rounded-3xl flex items-center justify-between cursor-pointer">
+                            Learn more{" "}
+                            <span className="p-1 text-[22px] ml-2 bg-white rounded-full text-black cursor-pointer">
+                              <MdOutlineArrowOutward />
+                            </span>
+                          </button>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <div className="p-10">
+                        <div className="py-5">
+                          <img
+                            className="w-[100%] h-[240px] rounded-3xl"
+                            src={img2}
+                            alt=""
+                          />
+                          <button className="bg-black  sliderRightButton w-[65%] text-white p-1 pl-5 rounded-3xl flex items-center justify-between cursor-pointer">
+                            Learn more{" "}
+                            <span className="p-1 text-[22px] ml-2 bg-white rounded-full text-black cursor-pointer">
+                              <MdOutlineArrowOutward />
+                            </span>
+                          </button>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <div className="p-10">
+                        <div className="py-5">
+                          <img
+                            className="w-[100%] h-[240px] rounded-3xl"
+                            src={img3}
+                            alt=""
+                          />
+                          <button className="bg-black  sliderRightButton w-[65%] text-white p-1 pl-5 rounded-3xl flex items-center justify-between cursor-pointer">
+                            Learn more{" "}
+                            <span className="p-1 text-[22px] ml-2 bg-white rounded-full text-black cursor-pointer">
+                              <MdOutlineArrowOutward />
+                            </span>
+                          </button>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <div className="p-10">
+                        <div className="py-5">
+                          <img
+                            className="w-[100%] h-[240px] rounded-3xl"
+                            src={img4}
+                            alt=""
+                          />
+                          <button className="bg-black  sliderRightButton w-[65%] text-white p-1 pl-5 rounded-3xl flex items-center justify-between cursor-pointer">
+                            Learn more{" "}
+                            <span className="p-1 text-[22px] ml-2 bg-white rounded-full text-black cursor-pointer">
+                              <MdOutlineArrowOutward />
+                            </span>
+                          </button>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <div className="p-10">
+                        <div className="py-5">
+                          <img
+                            className="w-[100%] h-[240px] rounded-3xl"
+                            src={img5}
+                            alt=""
+                          />
+                          <button className="bg-black  sliderRightButton w-[65%] text-white p-1 pl-5 rounded-3xl flex items-center justify-between cursor-pointer">
+                            Learn more{" "}
+                            <span className="p-1 text-[22px] ml-2 bg-white rounded-full text-black cursor-pointer">
+                              <MdOutlineArrowOutward />
+                            </span>
+                          </button>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                  </Swiper>
+                </div>
               </div>
-
-              <div className="w-[50%] h-full overflow-hidden">
-                <Swiper
-                  direction="vertical"
-                  modules={[Autoplay]}
-                  autoplay={{
-                    delay: 0,
-                    disableOnInteraction: false,
-                  }}
-                  speed={2000}
-                  loop={true}
-                  allowTouchMove={true}
-                  slidesPerView={3}
-                  spaceBetween={0}
-                  className="h-full w-full"
-                  style={{
-                    transform: "rotate(180deg)", // Flip the second swiper vertically to go bottom to top
-                    transformOrigin: "center", // Ensure the rotation happens around the center
-                  }}
-                >
-                  <SwiperSlide>
-                    <div className="text-white text-2xl p-10 "><div className="py-5"><img className="w-[100%] h-[240px] rounded-3xl" src={sliderimg} alt="" /></div></div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="text-white text-2xl p-10"><div className="py-5"><img className="w-[100%] h-[240px] rounded-3xl" src={sliderimg} alt="" /></div></div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="text-white text-2xl p-10"><div className="py-5"><img className="w-[100%] h-[240px] rounded-3xl" src={sliderimg} alt="" /></div></div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="text-white text-2xl p-10"><div className="py-5"><img className="w-[100%] h-[240px] rounded-3xl" src={sliderimg} alt="" /></div></div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="text-white text-2xl p-10"><div className="py-5"><img className="w-[100%] h-[240px] rounded-3xl" src={sliderimg} alt="" /></div></div>
-                  </SwiperSlide>
-                </Swiper>
+              <div>
+                <div className="flex flex-col gap-3 bg-gray-100 p-2 rounded-3xl">
+                  <span className="p-3 rounded-full bg-white text-black flex items-center justify-center cursor-pointer">
+                    <FaFacebookF />
+                  </span>
+                  <span className="p-3 rounded-full bg-white text-black flex items-center justify-center cursor-pointer">
+                    <FaInstagram />
+                  </span>
+                  <span className="p-3 h-10 rounded-full bg-white text-black flex items-center justify-center cursor-pointer">
+                    <FaLinkedinIn />
+                  </span>
+                </div>
               </div>
             </div>
           </div>
--- src/Components/HomeComponents/Nav.jsx
+++ src/Components/HomeComponents/Nav.jsx
@@ -4,19 +4,19 @@ import { MdOutlineArrowOutward } from 'react-icons/md'
 const Nav = () => {
   return (
     <>
-        <div className='flex justify-between py-5 px-15 '>
-            <div>
+        <div className='flex justify-between py-3 px-20 backdrop-blur-lg shadow-[0_10px_25px_rgba(0,0,0,0.10)]'>
+            <div className='flex items-center'>
                 <h3 className='navlogo relative font-semibold text-[22px]'>ChatRoom</h3>
             </div>
-            <div>
+            <div className='flex items-center'>
                 <ul className='flex gap-5 bg-gray-100 py-2 px-7 rounded-3xl'>
-                    <li>Home</li>
-                    <li>Home</li>
-                    <li>Home</li>
-                    <li>Home</li>
+                    <li>About</li>
+                    <li>Features</li>
+                    <li>Benefits</li>
+                    <li>Plan</li>
                 </ul>
             </div>
-            <div>
+            <div className='flex items-center'>
                 <button className='bg-gray-100 pl-4 p-1 rounded-3xl flex items-center'>Try out! <span className='p-2 text-[22px] ml-2 bg-green-200 rounded-full '><MdOutlineArrowOutward /></span></button>
             </div>
         </div>
