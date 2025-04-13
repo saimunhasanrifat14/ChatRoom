9a0087232e13a49b0a64f4e18e03feea9de2f3c6
Saimun Hasan <saimunhasanrifat14@gmail.com>
2025-04-14 01:22:08 +0600

    created benefits section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Nav from './HomeComponents/Nav'
 import Hero from './HomeComponents/Hero'
 import About from './HomeComponents/About'
+import Benefits from './HomeComponents/Benefits'
 
 const Home = () => {
   return (
    <>
     <Hero/>
     <About/>
+    <Benefits/>
    </>
   )
 }
---
+++ src/Components/HomeComponents/Benefits.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { FaHandHoldingHeart } from "react-icons/fa";
+
+const Benefits = () => {
+  return (
+    <>
+      <div className="w-full py-[80px]">
+        <div>
+          <h3 className="text-[50px] text-center">Benefits</h3>
+        </div>
+        <div className="w-full items-center px-15 pt-[50px]">
+          <div className="flex justify-between bg-white rounded-[45px] shadow-lg mb-[45px]">
+            <div className="w-[50%] py-15 px-10 flex items-center gap-15 ">
+              <span className="text-[22px] text-gray-300 p-3 bg-black rounded-full">
+                <FaHandHoldingHeart />
+              </span>
+              <div className=" flex flex-col gap-5 items-start">
+                <span className="text-[16px] py-2 px-7 border-[1px] border-solid border-gray-200 rounded-3xl">
+                  Lorem, ipsum dolor.
+                </span>
+                <p className="text-[18px]">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut
+                  quod amet corrupti, doloremque repellat officiis et. Deleniti
+                  blanditiis doloremque expedita corporis tenetur rerum
+                  quibusdam sed fugiat facilis blanditiis doloremque.
+                </p>
+              </div>
+            </div>
+            <div className="w-[50%] flex flex-col items-center gap-2 justify-center bg-gray-100 rounded-[45px]">
+              <h4>We are here to</h4>
+              <h2 className="text-[30px]">Elevate Engagement</h2>
+            </div>
+          </div>
+          <div className="flex justify-between bg-white rounded-[45px] shadow-lg mb-[45px]">
+            <div className="w-[50%] py-15 px-10 flex items-center gap-15 ">
+              <span className="text-[22px] text-gray-300 p-3 bg-black rounded-full">
+                <FaHandHoldingHeart />
+              </span>
+              <div className=" flex flex-col gap-5 items-start">
+                <span className="text-[16px] py-2 px-7 border-[1px] border-solid border-gray-200 rounded-3xl">
+                  Lorem, ipsum dolor.
+                </span>
+                <p className="text-[18px]">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut
+                  quod amet corrupti, doloremque repellat officiis et. Deleniti
+                  blanditiis doloremque expedita corporis tenetur rerum
+                  quibusdam sed fugiat facilis blanditiis doloremque.
+                </p>
+              </div>
+            </div>
+            <div className="w-[50%] flex flex-col items-center gap-2 justify-center bg-gray-100 rounded-[45px]">
+              <h4>We are here to</h4>
+              <h2 className="text-[30px]">Elevate Engagement</h2>
+            </div>
+          </div>
+          <div className="flex justify-between bg-white rounded-[45px] shadow-lg mb-[45px]">
+            <div className="w-[50%] py-15 px-10 flex items-center gap-15 ">
+              <span className="text-[22px] text-gray-300 p-3 bg-black rounded-full">
+                <FaHandHoldingHeart />
+              </span>
+              <div className=" flex flex-col gap-5 items-start">
+                <span className="text-[16px] py-2 px-7 border-[1px] border-solid border-gray-200 rounded-3xl">
+                  Lorem, ipsum dolor.
+                </span>
+                <p className="text-[18px]">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut
+                  quod amet corrupti, doloremque repellat officiis et. Deleniti
+                  blanditiis doloremque expedita corporis tenetur rerum
+                  quibusdam sed fugiat facilis blanditiis doloremque.
+                </p>
+              </div>
+            </div>
+            <div className="w-[50%] flex flex-col items-center gap-2 justify-center bg-gray-100 rounded-[45px]">
+              <h4>We are here to</h4>
+              <h2 className="text-[30px]">Elevate Engagement</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Benefits;
